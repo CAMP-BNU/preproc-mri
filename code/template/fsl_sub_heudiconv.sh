c905caa2f827d9f2bacf739fa88a0d37e0331d1d
Liang Zhang <psychelzh@outlook.com>
2023-04-18 19:20:03 +0800

    Support container
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- code/template/fsl_sub_heudiconv.sh
+++ code/template/fsl_sub_heudiconv.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+: ${SINGULARITY_CMD:=/opt/fmritools/singularity/bin/singularity}
+# heudiconv setup
+: ${HEUDICONV_CONTAINER:=/home/shaoxuhao2008/containers/neurobox-2.sif}
+: ${HEUDICONV_CMD:=${SINGULARITY_CMD} exec -c -e -B /seastor:/seastor ${HEUDICONV_CONTAINER} heudiconv}
 fsl_sub -l ${PROJECT_ROOT}/logs \
     -N heudiconv_site-${SITE}_sub-${SUBJECT}_ses-${SESSION} \
-    heudiconv -d ${PROJECT_ROOT}/sourcedata/${SITE}_*_CAMP_SUB{subject}_*_{session}/*/*/*.IMA \
+    ${HEUDICONV_CMD} -d ${PROJECT_ROOT}/sourcedata/${SITE}_*_CAMP_SUB{subject}_*_{session}/*/*/*.IMA \
     -o ${PROJECT_ROOT}/rawdata/${SITE} -s $SUBJECT -ss $SESSION \
     -f ${PROJECT_ROOT}/code/heuristic.py \
     -c dcm2niix -b --overwrite
