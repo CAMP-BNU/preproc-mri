d97072c46c452df47049c977444936c132d9a36f
Liang Zhang <psychelzh@outlook.com>
2023-04-20 16:33:10 +0800

    Reduce dependency
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- code/preprocess/correct-bids-json.R
+++ code/preprocess/correct-bids-json.R
@@ -2,21 +2,22 @@
 # https://github.com/nipreps/mriqc/issues/879
 # correct json files in place because there are too many files
 # the main goal is to remove all `"\u0000"`
-library(tidyverse)
-walk(fs::dir_ls(here::here("R")), source)
+purrr::walk(fs::dir_ls(here::here("R")), source)
 clean_json_embed <- function(file_origin,
                              file_corrected = NULL,
                              inplace = TRUE) {
-  content_origin <- read_file(file_origin)
-  content_corrected <- str_remove_all(content_origin, fixed("\\u0000"))
+  content_origin <- readr::read_file(file_origin)
+  content_corrected <- stringr::str_remove_all(
+    content_origin, stringr::fixed("\\u0000")
+  )
   if (!identical(content_origin, content_corrected)) {
     if (inplace) {
       file_corrected <- file_origin
       fs::file_chmod(file_origin, "644")
     }
-    write_file(content_corrected, file_corrected)
+    readr::write_file(content_corrected, file_corrected)
   }
 }
 
 files_json <- fs::dir_ls(path_raw, regexp = "json$", recurse = TRUE)
-walk(files_json, clean_json_embed, .progress = TRUE)
+purrr::walk(files_json, clean_json_embed, .progress = TRUE)
