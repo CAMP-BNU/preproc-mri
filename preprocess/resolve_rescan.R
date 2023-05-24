be83da1585aa8f66b209aaf2b731ebfdfcf5359c
Liang Zhang <psychelzh@outlook.com>
2023-05-24 20:08:54 +0800

    Add correction for more makeup sessions
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- preprocess/resolve_rescan.R
+++ preprocess/resolve_rescan.R
@@ -6,6 +6,12 @@
 
 library(tidyverse)
 
+correct_file_path <- function(file, file_new, ...) {
+  dir_new <- fs::path_dir(file_new)
+  if (!fs::dir_exists(dir_new)) fs::dir_create(dir_new)
+  fs::file_move(file, file_new)
+}
+
 # DWI correction ----
 
 #' * The `"dwi"` images are planned to be in the first session only, so if
@@ -33,9 +39,29 @@ recipe_dwi_correction <- tibble(
   )
 pwalk(
   recipe_dwi_correction,
-  \(file, file_new, ...) {
-    dir_new <- fs::path_dir(file_new)
-    if (!fs::dir_exists(dir_new)) fs::dir_create(dir_new)
-    fs::file_move(file, file_new)
-  }
+  correct_file_path
+)
+
+# Makeup ----
+
+#' * Sessions with number larger than 2 are makeups, so all the files there
+#' should be moved to the correct session. 3->1; 4->2. Now only 3 is found.
+#' It is also assumed that these files does not exist in the correct session.
+recipe_makeups <- tibble(
+  file = fs::dir_ls(
+    "rawdata",
+    regexp = "ses-3",
+    type = "file",
+    recurse = TRUE
+  )
+) |>
+  filter(fs::path_ext(file) != "tsv") |>
+  mutate(
+    file_new = stringr::str_replace_all(
+      file, "ses-3", "ses-1"
+    )
+  )
+pwalk(
+  recipe_makeups,
+  correct_file_path
 )
