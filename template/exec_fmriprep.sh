0006f73d0bae2f05aede94cc6d8f4310051ddadf
Liang Zhang <psychelzh@outlook.com>
2023-09-30 20:32:58 +0800

    Add more output spaces and aroma
    Fix #1
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- template/exec_fmriprep.sh
+++ template/exec_fmriprep.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
+# prepare command of fmriprep
 : ${SINGULARITY_CMD:=/opt/fmritools/singularity/bin/singularity}
 : ${FMRIPREP_CONTAINER:=/opt/fmritools/containers/fmriprep-23.0.2.sif}
 : ${FMRIPREP_CMD:=${SINGULARITY_CMD} run -c -e -B /seastor:/seastor -B /home/zhangliang:/home/zhangliang ${FMRIPREP_CONTAINER}}
+# positional parameters
+: ${INPUT_DIR:=${PROJECT_ROOT}/rawdata}
+: ${OUTPUT_DIR:=${PROJECT_ROOT}/derivatives/fmriprep}
+: ${WORK_DIR:=${PROJECT_ROOT}/tmp/fmriprep}
+# output spaces
+: ${OUTPUT_SPACES:=MNI152NLin2009cAsym MNI152NLin6Asym:res-2 anat fsaverage fsaverage6}
+# freesurfer
 : ${FS_LICENSE:=/seastor/zhangliang/license.txt}
+: ${FS_SUBJECTS_DIR:=${OUTPUT_DIR}/sourcedata/freesurfer}
+# performance related
 : ${NTHREADS:=8}
 : ${OMPTHREADS:=4}
 : ${MEMMB:=12288} # 12GiB
 
 ${FMRIPREP_CMD} \
-    ${PROJECT_ROOT}/rawdata ${PROJECT_ROOT}/derivatives/fmriprep participant \
+    ${INPUT_DIR} ${OUTPUT_DIR} participant \
     `# filtering bids queries` \
     --skip_bids_validation \
     --participant_label ${SUBJECT} \
@@ -16,9 +26,11 @@ ${FMRIPREP_CMD} \
     --nthreads $NTHREADS \
     --omp-nthreads $OMPTHREADS \
     --mem_mb $MEMMB \
-    --output-spaces MNI152NLin2009cAsym anat \
+    --output-spaces ${OUTPUT_SPACES} \
     `# other options` \
-    -w ${PROJECT_ROOT}/tmp/fmriprep \
+    -w ${WORK_DIR} \
     --stop-on-first-crash --notrack \
-    `# freesurfere license` \
-    --fs-license-file ${FS_LICENSE}
+    `# freesurfere options` \
+    --fs-license-file ${FS_LICENSE} --fs-subjects-dir ${FS_SUBJECTS_DIR} \
+    `# ICA denoise: note this option is deprecated in future versions` \
+    --use-aroma
