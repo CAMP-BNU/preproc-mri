1b0251482eae4a21e015d79282aba9d03c2cdae5
Liang Zhang <psychelzh@outlook.com>
2023-10-09 20:19:56 +0800

    Adds more variables for mriqc
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- template/exec_mriqc.sh
+++ template/exec_mriqc.sh
@@ -4,15 +4,22 @@
 : ${MRIQC_CONTAINER:=/opt/fmritools/containers/mriqc-23.1.0rc0.sif}
 # note $HOME is used for cached templateflow
 : ${MARQC_CMD:=${SINGULARITY_CMD} run -c -e -B /seastor:/seastor -B /home/zhangliang:/home/zhangliang ${MRIQC_CONTAINER}}
+# positional parameters
+: ${INPUT_DIR:=${PROJECT_ROOT}/rawdata}
+: ${OUTPUT_DIR:=${PROJECT_ROOT}/derivatives/mriqc}
+: ${BIDS_DATABASE_DIR:=${PROJECT_ROOT}/bids_db}
+: ${WORK_DIR:=${PROJECT_ROOT}/tmp}
+# performance related
 : ${NTHREADS:=8}
 : ${OMPTHREADS:=4}
 : ${MEMMB:=12288} # 12GiB
 
 ${MARQC_CMD} \
-    ${PROJECT_ROOT}/rawdata ${PROJECT_ROOT}/derivatives/mriqc participant \
+    ${INPUT_DIR} ${OUTPUT_DIR} participant \
     `# filtering subject` \
     --participant_label ${SUBJECT} \
     --session-id ${SESSION} \
+    --bids-database-dir ${BIDS_DATABASE_DIR} \
     `# instrumental options` \
     -w ${PROJECT_ROOT}/tmp \
     --write-graph --verbose-reports --no-sub \
