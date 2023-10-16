d74ed5546fe0682a872f254d808604c9b8742bad
Liang Zhang <psychelzh@outlook.com>
2023-10-16 23:59:31 +0800

    Suppress `load_all` messages
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- preprocess/perform_heudiconv.R
+++ preprocess/perform_heudiconv.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 project_root <- fs::path_dir(box::file())
 options(tidyverse.quiet = TRUE)
-devtools::load_all(project_root)
+devtools::load_all(project_root, quiet = TRUE)
 context <- "heudiconv"
 argv <- parse_arguments()
 prepare_jobs() |> extract_todo() |> execute_jobs()
