056957c652fe79ee6e242668e1f9e2fd1f7694da
Liang Zhang <psychelzh@outlook.com>
2024-07-19 00:03:56 +0800

    Update to xcp_d 0.7.5 with support for `--bids-database-dir`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- template/exec_xcpd.sh
+++ template/exec_xcpd.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # prepare command of fmriprep
 : ${SINGULARITY_CMD:=/opt/fmritools/singularity/bin/singularity}
-: ${XCPD_CONTAINER:=/opt/fmritools/containers/xcp_d-0.6.3.sif}
+: ${XCPD_CONTAINER:=/opt/fmritools/containers/xcp_d-0.7.5.sif}
 : ${XCPD_CMD:=${SINGULARITY_CMD} run -e -B /seastor,$HOME:/home/xcp_d --home /home/xcp_d ${XCPD_CONTAINER}}
 # positional parameters
 : ${CONFIG_PARAMS:=example}
 : ${FMRI_DIR:=${PROJECT_ROOT}/derivatives/fmriprep}
 : ${OUTPUT_DIR:=${PROJECT_ROOT}/derivatives/$CONFIG_PARAMS}
+: ${BIDS_DATABASE_DIR:=${PROJECT_ROOT}/derivatives/layout_fmriprep}
 : ${WORK_DIR:=${PROJECT_ROOT}/tmp}
 # freesurfer
 : ${FS_LICENSE:=/seastor/zhangliang/license.txt}
@@ -20,6 +21,7 @@ ${XCPD_CMD} \
     ${FMRI_DIR} ${OUTPUT_DIR} participant \
     `# filtering bids queries` \
     --participant_label ${SUBJECT} \
+    --bids-database-dir ${BIDS_DATABASE_DIR} \
     `# post-processing parameters` \
     $PARAMS_POST \
     `# performance options` \
